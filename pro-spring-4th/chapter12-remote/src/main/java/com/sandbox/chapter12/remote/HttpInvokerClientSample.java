8ea433561044a784599fc79ba8b5e2df07e9d390
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2017-06-25 19:35:34 +0300

    Chapter12: Added http client invoker

---
+++ pro-spring-4th/chapter12-remote/src/main/java/com/sandbox/chapter12/remote/HttpInvokerClientSample.java
@@ -0,0 +1,35 @@
+package com.sandbox.chapter12.remote;
+
+import com.sandbox.chapter12.remote.models.Contact;
+import com.sandbox.chapter12.remote.services.ContactService;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by andrii on 10.06.17.
+ */
+public class HttpInvokerClientSample {
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/spring/http-invoker-app-context.xml");
+        ctx.refresh();
+
+        ContactService contactService = ctx.getBean("remoteContactService", ContactService.class);
+        System.out.println("Finding all contacts");
+        List<Contact> contacts = contactService.findAll();
+        listContacts(contacts);
+        System.out.println("Finding contact with first name equals Chris");
+        contacts = contactService.findByFirstName("Chris");
+        listContacts(contacts);
+    }
+
+    private static void listContacts(List<Contact> contacts) {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+        System.out.println("");
+    }
+
+}
