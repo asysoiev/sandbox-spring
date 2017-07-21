ee8e22fc05afb0137834cb16728fb725fe76374b
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2017-07-21 20:24:15 +0300

    Chapter12: CRUD operations

--- pro-spring-4th/chapter12-rest/src/main/java/com/sandbox/chapter12/rest/ClientApp.java
+++ pro-spring-4th/chapter12-rest/src/main/java/com/sandbox/chapter12/rest/ClientApp.java
@@ -2,6 +2,7 @@ package com.sandbox.chapter12.rest;
 
 import com.sandbox.chapter12.rest.model.Contact;
 import com.sandbox.chapter12.rest.model.Contacts;
+import org.joda.time.DateTime;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,9 +23,36 @@ public class ClientApp {
         ctx.load("classpath:META-INF/spring/restful-client-app-context.xml");
         ctx.refresh();
         RestTemplate restTemplate = ctx.getBean("restTemplate", RestTemplate.class);
-        System.out.println("Testing retrieve all contacts:");
+
+        System.out.println("==========Get all contacts==========");
         Contacts contacts = restTemplate.getForObject(URL_GET_ALL_CONTACTS, Contacts.class);
         listContacts(contacts);
+
+        System.out.println("==========Get Contact by ID==========");
+        Contact contact = restTemplate.getForObject(URL_GET_CONTACT_BY_ID, Contact.class, 1);
+        System.out.println(contact);
+
+        System.out.println("==========Update Contact==========");
+        contact = restTemplate.getForObject(URL_UPDATE_CONTACT, Contact.class, 1);
+        contact.setFirstName("John Doe");
+        restTemplate.put(URL_UPDATE_CONTACT, contact, 1);
+        System.out.println("Updated Contact: " + contact);
+
+        System.out.println("==========Delete Contact==========");
+        restTemplate.delete(URL_DELETE_CONTACT, 1);
+        System.out.println("Testing delete contact by id :");
+        contacts = restTemplate.getForObject(URL_GET_ALL_CONTACTS, Contacts.class);
+        listContacts(contacts);
+
+        System.out.println("==========Create Contact==========");
+        System.out.println("Testing create contact :");
+        Contact contactNew = new Contact();
+        contactNew.setFirstName("James");
+        contactNew.setLastName("Gosling");
+        contactNew.setBirthDate(new DateTime());
+        contactNew =
+                restTemplate.postForObject(URL_CREATE_CONTACT, contactNew, Contact.class);
+        System.out.println("Contact created successfully: " + contactNew);
     }
 
     private static void listContacts(Contacts contacts) {
